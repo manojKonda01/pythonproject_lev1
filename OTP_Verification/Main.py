9fff24f74ca94e0960544ce0976aaa292aa9e4bb
Manoj Konda <81737699+manojKonda01@users.noreply.github.com>
2021-06-20 13:12:16 +0530

    Update Main.py

--- OTP_Verification/Main.py
+++ OTP_Verification/Main.py
@@ -2,12 +2,11 @@ import smtplib
 import email_msg
 import authentication
 from getpass import getpass
-
 # sender email and password check
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
-sender_email = "jackyspirrel@gmail.com"
-password = getpass("Type your password and press enter: ")
+sender_email = input("Enter Your Gmail!! Sender :")
+password = getpass("Sender enter Your password : ")
 
 
 # Try to log in to server and send email
@@ -15,9 +14,11 @@ password = getpass("Type your password and press enter: ")
 server = smtplib.SMTP(smtp_server, port)
 server.starttls()
 server.login(sender_email, password)
-receiver_email = input('Enter Your Email : ')
-server.sendmail('&&&&&&&&&&&', receiver_email, email_msg.message )
+receiver_email = input("Enter Receiver's Email : ")
+server.sendmail(sender_email, receiver_email, email_msg.message )
 
 print("Otp Sent to your Email please Check your email")
 
 authentication.authen()
+
+server.sendmail(sender_email, receiver_email, email_msg.mes)
